24851e6a7ba3452565256c764bb0be15392a850a
mingmoth <mingmoth@gmail.com>
2021-09-06 17:06:06 +0800

    restaurant route render

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const restaurantList = require('./restaurant.json')
+
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('This is my movie list built with Express')
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.filter(restaurant => restaurant.id == req.params.restaurant_id)
+  res.render('show', {
+    restaurant: restaurant[0]
+  })
 })
 
 app.listen(port, () => {
